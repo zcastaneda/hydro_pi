a2e35cb867760d76c1b83865c4faf3635b219b3d
Zack Castaneda <zcastaneda@gmail.com>
2022-12-03 23:48:20 -0800

    Add get_temp() method

---
+++ aws_iot/ds18b20_test.py
@@ -0,0 +1,45 @@
+import subprocess
+import re
+
+
+class pyds18b20:
+    def __init__(self) -> None:
+        
+        pass
+
+    def init_one_wire(self,GPIO_PIN):
+        #setup one wire
+
+        bash_cmd = f'sudo dtoverlay w1-gpio gpiopin={GPIO_PIN} pullup=0'
+        print(bash_cmd)
+        process = subprocess.Popen(bash_cmd.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        print(output,error)
+
+    def get_devices(self):
+        #refresh the list of devices available
+        bash_cmd = f'ls /sys/bus/w1/devices/'
+        process = subprocess.Popen(bash_cmd.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        device_list = output.decode('utf-8').split('\n')
+        r = re.compile('^28-.{12}')
+        self.device_list = list(filter(r.match, device_list))
+        print(device_list)
+        print(output,error)
+    
+    def get_temp(self):
+        for sensor_name in self.device_list:
+            bash_cmd = f'cat /sys/bus/w1/devices/{sensor_name}/temperature'
+            process = subprocess.Popen(bash_cmd.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            t_celsius = re.sub('\n','',output.decode('utf-8'))
+            t_celsius = round(float(t_celsius[0:len(t_celsius)-3]+'.'+t_celsius[-3:len(t_celsius)]),3)
+            t_farenheit = round((9/5)*t_celsius+32,3)
+            print(t_celsius,t_farenheit)
+
+
+if __name__ == "__main__":
+    t_sensor = pyds18b20()
+    t_sensor.init_one_wire(GPIO_PIN='17')
+    t_sensor.get_devices()
+    t_sensor.get_temp()
